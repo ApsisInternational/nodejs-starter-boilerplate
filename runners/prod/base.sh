1ed554bd680402c06dcda6694eb65fffcaf92e82
Jacek Marek <jacek.marek@apsis.com>
2017-04-12 11:05:07 +0200

    fix: dirname will now work no matter how runner scripts are run

--- runners/prod/base.sh
+++ runners/prod/base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 base="
     docker-compose
         -p api
--- runners/prod/up.sh
+++ runners/prod/up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/base.sh
 
 $base up -d api
